f05c3e31246ae267f99050fdf60b7d20a91be3d7
EduardoRad <edu98765@gmail.com>
2026-08-22 17:55:27 +0200

    Fixed alu module

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -14,21 +14,23 @@ module alu #(
 );
 
   always_comb begin
-    ALU_ADD: result_o = operand_a_i + operand_b_i;
-    ALU_SUB: result_o = operand_a_i - operand_b_i;
-    ALU_ADD: result_o = operand_a_i & operand_b_i;
-    ALU_OR: result_o = operand_a_i | operand_b_i;
-    ALU_XOR: result_o = operand_a_i ^ operand_b_i;
-    ALU_SLL  : result = operand_a << operand_b[4:0];
-    ALU_SRL  : result = operand_a >> operand_b[4:0];
-    ALU_SRA  : result = $signed(operand_a) >>> operand_b[4:0];
-    ALU_SLT  : result = {{(WIDTH-1){1'b0}}, less};
-    ALU_SLTU : result = {{(WIDTH-1){1'b0}}, less_u};
-    default: result_o = '0;
+    case (alu_op)
+      ALU_ADD: result_o = operand_a_i + operand_b_i;
+      ALU_SUB: result_o = operand_a_i - operand_b_i;
+      ALU_AND: result_o = operand_a_i & operand_b_i;
+      ALU_OR: result_o = operand_a_i | operand_b_i;
+      ALU_XOR: result_o = operand_a_i ^ operand_b_i;
+      ALU_SLL  : result_o = operand_a_i << operand_b_i[4:0];
+      ALU_SRL  : result_o = operand_a_i >> operand_b_i[4:0];
+      ALU_SRA  : result_o = $signed(operand_a_i) >>> operand_b_i[4:0];
+      ALU_SLT  : result_o = {{(WIDTH-1){1'b0}}, less};
+      ALU_SLTU : result_o = {{(WIDTH-1){1'b0}}, less_u};
+      default: result_o = '0;
+    endcase
   end
 
-  assign zero = (result_o == '0);
-  assign less = ($signed(operand_a) < $signed(operand_b));
-  assign less_u = (operand_a < operand_b);
+  assign zero = (operand_a_i == operand_b_i);
+  assign less = ($signed(operand_a_i) < $signed(operand_b_i));
+  assign less_u = (operand_a_i < operand_b_i);
 
 endmodule : alu

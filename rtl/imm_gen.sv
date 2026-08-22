32d8f3179f1175a2daa2eec78a2b52455f758b9d
EduardoRad <edu98765@gmail.com>
2026-08-22 20:07:54 +0200

    Fixed error in imm_gen module

--- rtl/imm_gen.sv
+++ rtl/imm_gen.sv
@@ -16,7 +16,7 @@ module imm_gen (
       IMM_B: imm_o = { {19{signed_ext}}, instr_i[31], instr_i[7], instr_i[30:25], instr_i[11:8], 1'b0 };
       IMM_U: imm_o = { instr_i[31:12], 12'b0 };
       IMM_J: imm_o = { {11{signed_ext}}, instr_i[31], instr_i[19:12], instr_i[20], instr_i[30:21], 1'b0 };
-      default : imm = '0;
+      default : imm_o = '0;
     endcase
   end
 
